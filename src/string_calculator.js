e9f53b813321b0e7669c8f3f54aa1746f837bf30
Paulartn <if24b296@technikum-wien.at>
2026-05-08 11:18:00 +0200

    Implemented sum of 3 numbers Testcase 5

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -8,8 +8,13 @@ StringCalculator.prototype.add = function(string_numbers) {
 	else if(string_numbers.includes(','))
 	{
 		let numbers = string_numbers.split(',');
+		let sum = 0;
 
-		return parseInt(numbers[0]) + parseInt(numbers[1]);
+		for (let i = 0; i < numbers.length; i++)
+		{
+			sum += parseInt(numbers[i], 10);
+		}
+		return sum;
 	}
 	else
 	{
