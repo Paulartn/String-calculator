ab2087f57a572881d7cb80570ca775aa643a7dd4
Paulartn <if24b296@technikum-wien.at>
2026-05-08 11:04:34 +0200

    Implemented sum of 2 numbers Testcase 3

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -5,6 +5,12 @@ StringCalculator.prototype.add = function(string_numbers) {
 	{
 		return 0;
 	} 
+	else if(string_numbers.includes(','))
+	{
+		let numbers = string_numbers.split(',');
+
+		return parseInt(numbers[0]) + parseInt(numbers[1]);
+	}
 	else
 	{
 		return parseInt(string_numbers);
